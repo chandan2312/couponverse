80c7a9cc1fd3dc7f8692c7e515b96d4a19cd80b2
chandan2312 <chandanchaudhari2312@gmail.com>
2024-07-07 20:54:17 +0530

    starter

--- packages/ui/actions/auth.js
+++ packages/ui/actions/auth.js
@@ -8,162 +8,162 @@ import bcrypt from "bcrypt";
 import { cookies } from "next/headers";
 
 const generateAccessAndRefereshTokens = async (userId) => {
-	const user = await prisma.user.findUnique({
-		where: {
-			id: userId,
-		},
-	});
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
 
-	if (!user) {
-		throw new Error("User not found");
-	}
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-	const accessToken = jwt.sign(
-		{
-			userId: user.id,
-			email: user.email,
-			username: user.username,
-		},
-		process.env.ACCESS_TOKEN_SECRET,
-		{
-			expiresIn: "1d",
-		}
-	);
+  const accessToken = jwt.sign(
+    {
+      userId: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
 
-	const refreshToken = jwt.sign(
-		{
-			userId: user.id,
-			email: user.email,
-			username: user.username,
-		},
-		process.env.REFRESH_TOKEN_SECRET,
-		{
-			expiresIn: "7d",
-		}
-	);
+  const refreshToken = jwt.sign(
+    {
+      userId: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
 
-	user.refreshToken = refreshToken;
+  user.refreshToken = refreshToken;
 
-	await prisma.user.update({
-		where: { id: user.id },
-		data: { refreshToken },
-	});
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { refreshToken },
+  });
 
-	return { accessToken, refreshToken };
+  return { accessToken, refreshToken };
 };
 
 export const checkAlreadyRegistered = async (email) => {
-	const user = await prisma.user.findUnique({
-		where: {
-			email: email,
-		},
-	});
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-	if (user) {
-		return { registered: true, data: user };
-	} else {
-		return { registered: false, data: null };
-	}
+  if (user) {
+    return { registered: true, data: user };
+  } else {
+    return { registered: false, data: null };
+  }
 };
 
 export const decodeAccessToken = async () => {
-	try {
-		const cookieStore = cookies();
-		const accessToken = cookieStore?.get("accessToken");
-		if (!accessToken) return;
-		const data = jwt.verify(accessToken.value, process.env.ACCESS_TOKEN_SECRET);
+  try {
+    const cookieStore = cookies();
+    const accessToken = cookieStore?.get("accessToken");
+    if (!accessToken) return;
+    const data = jwt.verify(accessToken.value, process.env.ACCESS_TOKEN_SECRET);
 
-		if (!data) return { status: 401, message: "Unauthorized" };
+    if (!data) return { status: 401, message: "Unauthorized" };
 
-		let user;
-		if (typeof data === "object" && "id" in data) {
-			user = await prisma.user.findUnique({
-				where: {
-					id: data.id,
-				},
-				select: {
-					id: true,
-					username: true,
-					email: true,
-					fullName: true,
-					avatar: true,
-				},
-			});
-		}
+    let user;
+    if (typeof data === "object" && "id" in data) {
+      user = await prisma.User.findUnique({
+        where: {
+          id: data.id,
+        },
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          fullName: true,
+          avatar: true,
+        },
+      });
+    }
 
-		if (!user) return { status: 401, data: null, message: "Unauthorized" };
-		return { status: 200, data };
-	} catch (error) {
-		return { status: 401, data: null, message: "Unauthorized" };
-	}
+    if (!user) return { status: 401, data: null, message: "Unauthorized" };
+    return { status: 200, data: user, message: "Authorized" };
+  } catch (error) {
+    return { status: 401, data: null, message: "Unauthorized" };
+  }
 };
 
 export const register = async (data) => {
-	const isRegistered = await checkAlreadyRegistered(data.email);
+  const isRegistered = await checkAlreadyRegistered(data.email);
 
-	let currUser;
-	let isNewlyRegistered = false;
-	if (!isRegistered.registered) {
-		isNewlyRegistered = true;
-		currUser = await prisma.user.create({
-			data: data,
-		});
-	} else {
-		currUser = isRegistered?.data;
-	}
+  let currUser;
+  let isNewlyRegistered = false;
+  if (!isRegistered.registered) {
+    isNewlyRegistered = true;
+    currUser = await prisma.user.create({
+      data: data,
+    });
+  } else {
+    currUser = isRegistered?.data;
+  }
 
-	// data.ip4 = ip();
-	// data.ip6 = ipv6();
+  // data.ip4 = ip();
+  // data.ip6 = ipv6();
 
-	const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
-		currUser?.id
-	);
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    currUser?.id,
+  );
 
-	cookies().set({
-		name: "refreshToken",
-		value: refreshToken,
-		httpOnly: true,
-		secure: true,
-		maxAge: 60 * 60 * 24 * 7,
-		secure: process.env.NODE_ENV === "production",
-		maxAge: 60 * 60 * 24 * 7,
-		path: "/",
-		domain: ".dealcodie.com",
-		sameSite: "None",
-	});
-	cookies().set({
-		name: "accessToken",
-		value: accessToken,
-		httpOnly: true,
-		secure: true,
-		maxAge: 60 * 60 * 24 * 7,
-		secure: process.env.NODE_ENV === "production",
-		maxAge: 60 * 60 * 24 * 7,
-		path: "/",
-		domain: ".dealcodie.com",
-		sameSite: "None",
-	});
+  cookies().set({
+    name: "refreshToken",
+    value: refreshToken,
+    httpOnly: true,
+    secure: true,
+    maxAge: 60 * 60 * 24 * 7,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 7,
+    path: "/",
+    domain: ".dealcodie.com",
+    sameSite: "None",
+  });
+  cookies().set({
+    name: "accessToken",
+    value: accessToken,
+    httpOnly: true,
+    secure: true,
+    maxAge: 60 * 60 * 24 * 7,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 7,
+    path: "/",
+    domain: ".dealcodie.com",
+    sameSite: "None",
+  });
 
-	return {
-		status: 200,
-		isNewlyRegistered: isNewlyRegistered,
-		data: currUser,
-		message: "Successfully Login",
-		accessToken,
-		refreshToken,
-	};
+  return {
+    status: 200,
+    isNewlyRegistered: isNewlyRegistered,
+    data: currUser,
+    message: "Successfully Login",
+    accessToken,
+    refreshToken,
+  };
 };
 
 export const tokenChecker = async () => {
-	const token = cookies().get("accessToken");
-	if (!token) {
-		return { status: 401, message: "Unauthorized" };
-	}
-	try {
-		const data = jwt.verify(token.value, process.env.ACCESS_TOKEN_SECRET);
-		return { status: 200, data };
-	} catch (error) {
-		console.log(error);
-		return { status: 401, message: "Unauthorized" };
-	}
+  const token = cookies().get("accessToken");
+  if (!token) {
+    return { status: 401, message: "Unauthorized" };
+  }
+  try {
+    const data = jwt.verify(token.value, process.env.ACCESS_TOKEN_SECRET);
+    return { status: 200, data };
+  } catch (error) {
+    console.log(error);
+    return { status: 401, message: "Unauthorized" };
+  }
 };
